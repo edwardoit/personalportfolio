3c8de91ecaa69a6665436d99f2cdccf2a7c0919b
Edoardo Cresci <edoardo.cresci@alten.it>
2022-12-12 15:13:23 +0100

    add public url

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./customization/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+
 
 //seleziono il div root nell'index.html
 const root = ReactDOM.createRoot(
@@ -12,9 +12,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-      <HashRouter>
           <App />
-      </HashRouter>
   </React.StrictMode>
 );
 
